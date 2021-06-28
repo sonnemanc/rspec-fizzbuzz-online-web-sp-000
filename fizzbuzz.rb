d7c391e97d663b80b094aa06927d2395ca7c5de8
Corban <sonnemanc@gmail.com>
2021-06-28 17:54:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,13 @@
-def fizzbuss
-  if number % 3 == 0
-   puts "Fizz"
- end
- if number % 5 == 0
-   puts "Buzz"
- end
+def fizzbuzz(last_number)
+  1.upto(last_number).each do |x|              
+    if (x % 3 == 0) && (x % 5 == 0)
+      puts " Fizzbuzz"
+    elsif x % 3 == 0
+      puts " Fizz"
+    elsif x % 5 == 0
+      puts " Buzz"
+    else
+      puts x
+    end
+  end
 end
