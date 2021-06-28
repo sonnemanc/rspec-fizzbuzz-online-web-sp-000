c5e29a51e80612f06b8433fc3c48dc3de99b0fd0
Corban <sonnemanc@gmail.com>
2021-06-28 17:51:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
+def fizzbuzz(n)
+  (1..n).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts 'fizzbuzz'
+    elsif i % 3 == 0
+      puts 'fizz'
+    elsif i % 5 == 0
+      puts 'buzz'
+    else
+      puts i
+    end
+  end
 end
+
+fizzbuzz(100)
