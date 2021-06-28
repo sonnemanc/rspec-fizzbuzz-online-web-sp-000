318677a8d0ca674e64328c10371d52eb86deacb6
Corban <sonnemanc@gmail.com>
2021-06-28 17:57:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(last_number)
   1.upto(last_number).each do |x|
     if (x % 3 == 0) && (x % 5 == 0)
-      puts " Fizzbuzz"
+      puts " FizzBuzz"
     elsif x % 3 == 0
       puts " Fizz"
     elsif x % 5 == 0
